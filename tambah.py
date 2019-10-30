d95d82563ebb8ed11fc747e3eb7f60be135d596b
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-10-30 16:24:28 +0700

    Create tambah.py

---
+++ tambah.py
@@ -0,0 +1,16 @@
+# Pertambahan
+
+import os, sys, time
+
+def write(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+print
+write ('\033[0m[\033[91;1m!\033[0m] \033[1;77mChecking...')
+print
+print ('\033[0m[\033[96;1m#\033[0m] \033[1;77mPertambahan')
+number1 = input('\033[0m[\033[95;1m+\033[0m] Enter numbers : \033[1;77m')
+number2 = input('\033[0m[\033[95;1m+\033[0m] Plus how much? : \033[1;77m')
+print (number1 + number2)
