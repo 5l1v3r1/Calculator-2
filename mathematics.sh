2e7043badf9864248e129ad570d229d9eb1179b6
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-11-13 19:49:22 +0700

    Update mathematics.sh

--- mathematics.sh
+++ mathematics.sh
@@ -1,41 +1,80 @@
-#!/usr/bin/shell
-# Tools Mathematics
+#!/usr/bin/bash
+# Tools Aritmatika
 # Coded by Senja
 # Github: github.com/thesixtynine/Mathematics
+
+checkroot() {
+
+if [[ "$(id -u)" -ne 0 ]]; then
+    printf "\e[1;77mPlease, run this program as root!\n\e[0m"
+    exit 1
+fi
+
+}
+
+checktor() {
+
+check=$(curl --socks5-hostname localhost:9050 -s https://www.google.com > /dev/null; echo $?)
+
+if [[ "$check" -gt 0 ]]; then
+    printf "\e[1;77mPlease, check your TOR Connection! Just type \"tor\" or \"service tor start\"\n\e[0m"
+    exit 1
+
+fi
+
+}
+
+clearscreen() {
 
 clear
 reset
 sleep 1
 
-printf '\033[1;77m
+}
+
+dependencies() {
+
+command -v nano > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
+command -v python > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
+
+}
+
+banner() {
+
+printf '\e[1;77m
       __  ___     __  __                  __  _
      /  |/  /__ _/ /_/ /  ___ __ _  ___ _/ /_(_)______
     / /|_/ / _ `/ __/ _ \/ -_)  ` \/ _ `/ __/ / __(_-<
    /_/  /_/\_,_/\__/_//_/\__/_/_/_/\_,_/\__/_/\__/___/
 
-\033[0;37;45m ○● 千尺丨乇几ᗪ丂 ㄒ卄乇 Ꮆ尺卂ㄚ 卄卂ㄒ 爪ㄚ ㄒ乇卂爪 ●○ \033[0m
-
-
 '
 
-sleep 1
-echo "\033[0m[\033[94;1m#\033[0m] \033[0mTools Aritmatika"
-echo "\033[0m[\033[93;1m*\033[0m] \033[0mCoded by Senja"
-echo "\033[0m[\033[96;1m#\033[0m] \033[0mMy Github: @thesixtynine"
-sleep 1
+}
+
+template() {
+
 echo
-echo
-echo "\033[0m[\033[92;1m1\033[0m] \033[77;1mPertambahan";
-echo "\033[0m[\033[92;1m2\033[0m] \033[77;1mPengurangan";
-echo "\033[0m[\033[92;1m3\033[0m] \033[77;1mPerkalian";
-echo "\033[0m[\033[92;1m4\033[0m] \033[77;1mPembagian";
-echo "\033[0m[\033[92;1m5\033[0m] \033[77;1mPemangkatan";
-echo "\033[0m[\033[92;1m0\033[0m] \033[77;1mExit";
+printf "\e[0m[\e[94;1m#\e[0m] Tools Aritmatika\n"
+printf "\e[0m[\e[93;1m*\e[0m] Coded by Senja\n"
+printf "\e[0m[\e[96;1m#\e[0m] My Github: @thesixtynine\n"
+sleep 1
 echo
 
-read -p "[/] Select option : " choice;
+}
 
-case $choice in
+menu() {
+
+printf "\e[0m[\e[92;1m1\e[0m] \e[77;1mPertambahan\n";
+printf "\e[0m[\e[92;1m2\e[0m] \e[77;1mPengurangan\n";
+printf "\e[0m[\e[92;1m3\e[0m] \e[77;1mPerkalian\n";
+printf "\e[0m[\e[92;1m4\e[0m] \e[77;1mPembagian\n";
+printf "\e[0m[\e[92;1m5\e[0m] \e[77;1mPemangkatan\n";
+printf "\e[0m[\e[92;1mQ\e[0m] \e[77;1mQuotes\n";
+printf "\e[0m[\e[92;1m0\e[0m] \e[77;1mExit\n";
+
+read -p $'\n\e[0m[\e[1;95m/\e[0m\e[0m] \e[1;77mSelect an option: \e[0m' option;
+
+case $option in
 
 1) python2 tambah.py
 
@@ -57,11 +96,30 @@ case $choice in
 
 ;;
 
-0) python text.py
-
+0) printf "\n\e[0m[\033[91;1m!\e[0m] \e[1;77mExit the program!\n\n"
 exit 1
 
 ;;
 
-*) echo "\033[0m[\033[91;1m!\033[0m] \033[1;77mInvalid option"
+Q) python text.py
+exit
+
+;;
+
+*) printf "\n\e[0m[\033[91;1m!\e[0m] \e[1;77mInvalid option!\n"
+sleep 1
+clearscreen
+banner
+template
+menu
 esac
+
+}
+
+#checkroot
+#checktor
+#dependencies
+clearscreen
+banner
+template
+menu
